dacec7ea8b961cf6f271232ddade664ac62f0294
afifbashair <mafifbashair@gmail.com>
2026-05-12 20:55:23 +0700

    tugas 3

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const PORT = process.env.PORT || 8080;
+
 const app = express();
 
 app.use(cors());
@@ -19,5 +21,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Server running ');
+  console.log('Server running on port ${PORT}');
 });
