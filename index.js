b786277acd4850cddeeaac9159b1c58b5d8da839
afifbashair <mafifbashair@gmail.com>
2026-05-13 07:43:13 +0700

    add .yaml

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -20,6 +20,6 @@ app.get('/', (req, res) => {
   res.send('API Notes jalan 🚀');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server running on port ${PORT}');
 });
