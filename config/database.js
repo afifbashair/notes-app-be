eb8fb2ae38d9447a7dccd3468e976c8c14a3eb1f
afifbashair <mafifbashair@gmail.com>
2026-05-13 09:14:26 +0700

    update: config (ssl)

--- config/database.js
+++ config/database.js
@@ -2,23 +2,16 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME, 
-  process.env.DB_USER, 
-  process.env.DB_PASS, 
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    dialectOptions: {
-      // Pertemuan 2
-      // logging: false,
-      // Pertemuan 3: Jika menggunakan Cloud SQL, tambahkan opsi SSL
-      ssl: {
-        
-        rejectUnauthorized: false,
-      },
-    },
+    logging: false,
   }
 );
+
 console.log('USER:', process.env.DB_USER);
-console.log('PASS:', process.env.DB_PASS);
+
 module.exports = sequelize;
