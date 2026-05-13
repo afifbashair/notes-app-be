78a4a6d18fc81f1926f8c4f933611cb95f6b4646
afifbashair <mafifbashair@gmail.com>
2026-05-13 07:54:41 +0700

    add .yaml

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ app.get('/', (req, res) => {
   res.send('API Notes jalan 🚀');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log('Server running on port ${PORT}');
 });
